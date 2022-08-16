d0a621ffc3198d09d7abff43726614c27a652955
hrshuv0 <h.r.shuvo9@gmail.com>
2022-08-16 11:49:39 +0600

    added repository pattern

---
+++ Medical Inventory/Data/IRepository/IRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Linq.Expressions;
+
+namespace Medical_Inventory.Data.IRepository;
+
+public interface IRepository<T> where T: class
+{
+    T? GetFirstOrDefault(Expression<Func<T, bool>> filter);
+    IEnumerable<T> GetAll(Expression<Func<T>>? filter = null);
+    void Add(T entity);
+    void Remove(T entity);
+}
---
+++ Medical Inventory/Data/IRepository/Repository/Repository.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq.Expressions;
+
+namespace Medical_Inventory.Data.IRepository.Repository;
+
+public class Repository<T> : IRepository<T> where T : class
+{
+    public T? GetFirstOrDefault(Expression<Func<T, bool>> filter)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<T> GetAll(Expression<Func<T>>? filter = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Add(T entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Remove(T entity)
+    {
+        throw new NotImplementedException();
+    }
+}
