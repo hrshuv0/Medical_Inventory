7beda55707c94ed70274ca7e7e7cf446c7d0e23c
hrshuv0 <h.r.shuvo9@gmail.com>
2022-08-17 18:00:40 +0600

    stuck - trying to improve view  structure

--- Medical Inventory/Controllers/ProductsController.cs
+++ Medical Inventory/Controllers/ProductsController.cs
@@ -6,146 +6,164 @@ using Medical_Inventory.Data.IRepository;
 using Medical_Inventory.Models;
 using Medical_Inventory.Models.ViewModel;
 
-namespace Medical_Inventory.Controllers
+namespace Medical_Inventory.Controllers;
+
+public class ProductsController : Controller
 {
-    public class ProductsController : Controller
+    private readonly IProductRepository _productRepository;
+    private readonly ICategoryRepository _categoryRepository;
+
+    public ProductsController(IProductRepository productRepository, ICategoryRepository categoryRepository)
     {
-        private readonly IProductRepository _productRepository;
-        private readonly ICategoryRepository _categoryRepository;
-
-        public ProductsController(IProductRepository productRepository, ICategoryRepository categoryRepository)
-        {
-            _productRepository = productRepository;
-            _categoryRepository = categoryRepository;
-        }
-
-
-        // GET: Products
-        public async Task<IActionResult> Index(ProductVm? product, string? searchString=null)
-        {
-            var categoryList = _categoryRepository.GetAll().Result.Select(c => new SelectListItem()
-            {
-                Text = c.Name,
-                Value = c.Id.ToString()
-            });
-            // ViewData["CategoryId"] = new SelectList(categoryList, "Id", "Name");
-            
-            var productList = await _productRepository.GetAll(includeProperties:"Category");
-
-            if(searchString is not null)
-                productList = productList.Where(p => p.Name.ToLower().Contains(searchString.ToLower()));
-
-            if (product!.SelectedCategory is not null && product.SelectedCategory.ToLower() != "all")
-                productList = productList.Where(p => p.Category.Id.ToString() == product.SelectedCategory);
-            
-
-            var productVm = new ProductVm()
-            {
-                CategoryList = categoryList,
-                Products = productList
-            };
-            
-            
-
-            return View(productVm);
-        }
-
-        // GET: Products/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            var product = await _productRepository.GetFirstOrDefault(c => c.Id == id);
-
-            if (product == null || id == null)
-                return NotFound();
-
-            return View(product);
-        }
-
-        // GET: Products/Create
-        public IActionResult Create()
-        {
-            var categoryList = _categoryRepository.GetAll().Result;
-            ViewData["CategoryId"] = new SelectList(categoryList, "Id", "Name");
-            
-            return View();
-        }
-
-        // POST: Products/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Name,Strength,Generic,Details,CategoryId")] Product product)
-        {
-            if (ModelState.IsValid)
-            {
-                await _productRepository.Add(product);
-                await _productRepository.Save();
-                
-                return RedirectToAction(nameof(Index));
-            }
-            
-            return View(product);
-        }
-
-        // GET: Products/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            var product = await _productRepository.GetFirstOrDefault(p => p.Id == id);
-            if (product == null || id == null)
-            {
-                return NotFound();
-            }
-            
-            var categoryList = _categoryRepository.GetAll().Result;
-            ViewData["CategoryId"] = new SelectList(categoryList, "Id", "Name");
-            
-            return View(product);
-        }
-
-        // POST: Products/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Strength,Generic,Details,CategoryId")] Product product)
-        {
-            if (id != product.Id)
-            {
-                return NotFound();
-            }
-
-            if (!ModelState.IsValid) return View(product);
-
-            _productRepository.Update(product);
-            await _productRepository.Save();
-
-            return RedirectToAction(nameof(Index));
-        }
-
-        // GET: Products/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            var product = await _productRepository.GetFirstOrDefault(c => c.Id == id);
-
-            if (product == null || id == null)
-            {
-                return NotFound();
-            }
-
-            return View(product);
-        }
-
-        // POST: Products/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var product = await _productRepository.GetFirstOrDefault(c => c.Id == id);
-
-            if (product != null)
-            {
-                _productRepository.Remove(product);
-                await _categoryRepository.Save();
-            }
-
-            return RedirectToAction(nameof(Index));
-        }
+        _productRepository = productRepository;
+        _categoryRepository = categoryRepository;
     }
+
+
+    // GET: Products
+    public async Task<IActionResult> Index(ProductVm? product, string? searchString=null)
+    {
+        var categoryList = _categoryRepository.GetAll().Result.Select(c => new SelectListItem()
+        {
+            Text = c.Name,
+            Value = c.Id.ToString()
+        });
+        // ViewData["CategoryId"] = new SelectList(categoryList, "Id", "Name");
+            
+        var productList = await _productRepository.GetAll(includeProperties:"Category");
+
+        if(searchString is not null)
+            productList = productList.Where(p => p.Name.ToLower().Contains(searchString.ToLower()));
+
+        if (product!.SelectedCategory is not null && product.SelectedCategory.ToLower() != "all")
+            productList = productList.Where(p => p.Category.Id.ToString() == product.SelectedCategory);
+            
+        ViewData["filterValue"] = "all";
+
+        var productVm = new ProductVm()
+        {
+            CategoryList = categoryList,
+            Products = productList
+        };
+            
+            
+
+        return View(productVm);
+    }
+
+    // GET: Products/Details/5
+    public async Task<IActionResult> Details(int? id)
+    {
+        var product = await _productRepository.GetFirstOrDefault(c => c.Id == id);
+
+        if (product == null || id == null)
+            return NotFound();
+
+        return View(product);
+    }
+
+    // GET: Products/Create
+    public IActionResult Create()
+    {
+        var categoryList = _categoryRepository.GetAll().Result;
+        ViewData["CategoryId"] = new SelectList(categoryList, "Id", "Name");
+            
+        return View();
+    }
+
+    // POST: Products/Create
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create([Bind("Id,Name,Strength,Generic,Details,CategoryId")] Product product)
+    {
+        if (ModelState.IsValid)
+        {
+            await _productRepository.Add(product);
+            await _productRepository.Save();
+                
+            return RedirectToAction(nameof(Index));
+        }
+            
+        return View(product);
+    }
+
+    // GET: Products/Edit/5
+    public async Task<IActionResult> Edit(int? id)
+    {
+        var product = await _productRepository.GetFirstOrDefault(p => p.Id == id);
+        if (product == null || id == null)
+        {
+            return NotFound();
+        }
+            
+        var categoryList = _categoryRepository.GetAll().Result;
+        ViewData["CategoryId"] = new SelectList(categoryList, "Id", "Name");
+            
+        return View(product);
+    }
+
+    // POST: Products/Edit/5
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Strength,Generic,Details,CategoryId")] Product product)
+    {
+        if (id != product.Id)
+        {
+            return NotFound();
+        }
+
+        if (!ModelState.IsValid) return View(product);
+
+        _productRepository.Update(product);
+        await _productRepository.Save();
+
+        return RedirectToAction(nameof(Index));
+    }
+
+    // GET: Products/Delete/5
+    public async Task<IActionResult> Delete(int? id)
+    {
+        var product = await _productRepository.GetFirstOrDefault(c => c.Id == id);
+
+        if (product == null || id == null)
+        {
+            return NotFound();
+        }
+
+        return View(product);
+    }
+
+    // POST: Products/Delete/5
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> DeleteConfirmed(int id)
+    {
+        var product = await _productRepository.GetFirstOrDefault(c => c.Id == id);
+
+        if (product != null)
+        {
+            _productRepository.Remove(product);
+            await _categoryRepository.Save();
+        }
+
+        return RedirectToAction(nameof(Index));
+    }
+
+    
+
+
+    #region API CALLS
+
+    public async Task<IActionResult> GetAll(string? id)
+    {
+        var productList = await _productRepository.GetAll(includeProperties:"Category");
+        
+        if(id is not null && id.ToLower() != "all")
+            productList = productList.Where(p => p.CategoryId.ToString() == id);
+
+        return Json(new { data = productList });
+    }
+
+    #endregion
+    
 }
